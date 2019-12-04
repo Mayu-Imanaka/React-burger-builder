4bc96d72bc1a3888b4e631d364b05f62a867ffa5
Mayu Imanaka <imamayu.1215@gmail.com>
2019-12-04 13:04:04 -0800

    update:output burger ingredients

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import classes from './BurgerIngredi'
+import classes from './BurgerIngredient.css'
 
 class BurgerIngredient extends Component {
     render() {
@@ -14,8 +14,8 @@ class BurgerIngredient extends Component {
             case ('bread-top'):
                 ingredient = (
                     <div className={classes.BreadTop}>
-                        <div className={classes.Seed1}></div>
-                        <div className={classes.Seed2}></div>
+                        <div className={classes.Seeds1}></div>
+                        <div className={classes.Seeds2}></div>
                     </div>
                 );
                 break;
